9899849025dfa44d21d255aa54067d94cb62b15e
Evan Oicle <oicleevan@protonmail.com>
2021-04-17 10:47:05 -0400

    Minor changes

--- runmenu.cpp
+++ runmenu.cpp
@@ -15,24 +15,23 @@ int main()
     Modifier blue(Color::BG_DARKBLUE);
     Modifier def(Color::BG_DEFAULT);
     
-    CALL:
-    cout << blue << " Run: " << def << " ";
-    getline(cin, input);
+    while(true) {
+    	cout << blue << " Run: " << def << " ";
+    	getline(cin, input);
 
-    if(input == "" | input == " ")
-    {
-        system("echo there is nothing to do");
-        system("sleep 3");
-        system("clear");
-        
-        goto CALL;
-    } else {
-        string program = "nohup " + input + " &";
-
-        system(program.c_str());
-        system("exit");
-        system("rm ./nohup.out");
+    	if(input == "" | input == " ")
+    	{
+            system("echo there is nothing to do");
+	    system("sleep 3");
+	    system("clear");    
+    	} else break;
     }
+
+    string program = "nohup " + input + " &";
+
+    system(program.c_str());
+    system("exit");
+    system("rm ./nohup.out");
 }
 
     
